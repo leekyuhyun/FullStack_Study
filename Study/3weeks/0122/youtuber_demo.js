43299260cbf20189f0a24a87e00e6422746cd53c
leekyuhyun <leekh010502@gmail.com>
2026-01-23 13:32:30 +0900

    fix : 0122 실습파일 수정

--- Study/3weeks/0122/youtuber_demo.js
+++ Study/3weeks/0122/youtuber_demo.js
@@ -2,29 +2,28 @@ const express = require("express");
 const app = express();
 app.listen(3001);
 
-let youtuber = {
-  user01: {
-    channelTitle: "SSG랜더스",
-    subscribers: "21.4만명",
-    videoNum: "4.2천개",
-  },
-  user02: {
-    channelTitle: "튜브김민교",
-    subscribers: "77.6만명",
-    videoNum: "1.9천개",
-  },
-  user03: {
-    channelTitle: "두치와뿌꾸",
-    subscribers: "88.7만명",
-    videoNum: "1.9만개",
-  },
+let youtuber1 = {
+  channelTitle: "SSG랜더스",
+  subscribers: "21.4만명",
+  videoNum: "4.2천개",
+};
+
+let youtuber2 = {
+  channelTitle: "튜브김민교",
+  subscribers: "77.6만명",
+  videoNum: "1.9천개",
+};
+
+let youtuber3 = {
+  channelTitle: "두치와뿌꾸",
+  subscribers: "88.7만명",
+  videoNum: "1.9만개",
 };
 
 const db = new Map();
-db.set("@15ya", youtuber.user01);
-db.set("@chimchakman", youtuber.user02);
-db.set("@teo", youtuber.user03);
-
+db.set("@SSGLADERS", youtuber1);
+db.set("@Onepunchk1ng_mk", youtuber2);
+db.set("@두치와뿌꾸", youtuber3);
 // REST API 설계
 app.get("/youtuber/:id", (req, res) => {
   const { id } = req.params;

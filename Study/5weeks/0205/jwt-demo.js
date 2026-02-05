a109997cda2612ebac20aed03916ac427eaf0df3
leekyuhyun <leekh010502@gmail.com>
2026-02-05 17:31:36 +0900

    feat : 0205 실습파일 추가

---
+++ Study/5weeks/0205/jwt-demo.js
@@ -0,0 +1,14 @@
+var jwt = require("jsonwebtoken"); // jwt 모듈 소환
+var dotenv = require("dotenv"); // dotenv 모듈 소환
+
+dotenv.config({ path: "../../../.env" });
+
+// 서명 = 토큰 발행
+var token = jwt.sign({ foo: "bar" }, process.env.SECRET_KEY);
+// token 생성 = jwt 서명을 했다! (페이로드 + 나만의 암호키) + SHA256
+console.log(token);
+
+// 검증
+// 만약 검증에 성공하면, 페이로드 값을 확인할 수 있음
+var decoded = jwt.verify(token, process.env.SECRET_KEY);
+console.log(decoded);

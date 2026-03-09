ecb831008c5712069bc7c5961812b45aaf1bf734
leekyuhyun <leekh010502@gmail.com>
2026-03-09 20:12:42 +0900

    feat : 0309 함수 생성 실습파일 추가

---
+++ Study/9weeks/0309/CreateFunction.js
@@ -0,0 +1,14 @@
+const sum = (a, b) => a + b;
+console.log(sum(10, 5));
+
+const square = (x) => x * x;
+console.log(square(4));
+
+const getLength = (str) => str.length;
+console.log(getLength("hello"));
+
+const double = (x) => x * 2;
+console.log(double(7));
+
+const sayHi = () => "안녕하세요!";
+console.log(sayHi());
